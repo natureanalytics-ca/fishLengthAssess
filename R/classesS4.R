c62f3fd279f0cac57683ca13bceba4b137adebdf
natureanalytics-ca <bill@natureanalytics.ca>
2021-10-05 14:41:54 -0400

    Initial commit

---
+++ R/classesS4.R
@@ -0,0 +1,38 @@
+
+#----------------------
+#Length composition object
+#----------------------
+
+#Roxygen header
+#'Length composition object
+#'
+#'An S4 object that holds length composition data in a specified format.
+#'
+#'The length composition object is contains a data frame of length observations.
+#'These observations are organized according to 'dataType'. When 'dataType' is set to 'lengths', each row contains
+#'a length observation and columns represent discrete observation units, such as year.
+#'When 'dataType' is set to 'freq', each row is the number of length observations corresponding to a length bin.
+#'The first column must contain the mid-points of each length bin, with columns 2 and above representing discrete observation units
+#' @param title A title for the object, useful for displaying the contents of the object
+#' @param description A longer description of the object
+#' @param L_units Units of measure for object, e.g., cm
+#' @param dataType Options are lengths or freq. See details for additional information
+#' @param observationUnit Provide a description for interpreting column header, e.g., year
+#' @param header Logical. Whether dataframe contains a descriptive  header. FALSE will cause the header to be hidden
+#' @param dt The length composition data frame.
+#' @import LBSPR
+#' @import fishSimGTG
+#' @importFrom methods new
+#' @export
+
+setClass("LengthComp",
+         representation(
+           title = "character",
+           description = "character",
+           L_units = "character",
+           dataType = "character",
+           observationUnit = "character",
+           header = "logical",
+           dt = "data.frame")
+)
+
---
+++ R/fishLengthAssess.R
@@ -0,0 +1,18 @@
+
+
+#' Length-based evaluation of fish stock and fishery
+#'
+#' Evaluates stock status and fishery compliance, and conducts other management-related tasks based on length-frequency data.
+#'
+#' @docType package
+#' @name fishLengthAssess
+"_PACKAGE"
+
+#' Length composition example
+#'
+#' A LengthComp object, prepopulated with an example simulated from LBSPRsim.
+#'
+#'The life history used to generate lenth composition is based on the Beverton-Holt life history invariants of M/K=1.5 and Lm/Linf=0.66
+#' @format An S4 object of class LengthComp
+#' @source Simulated data
+"LengthCompExample"
