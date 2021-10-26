4e6047eabbc0afd89513d312c011e79950380817
natureanalytics-ca <bill@natureanalytics.ca>
2021-10-26 01:19:42 -0400

    Updates

--- data-raw/lengthCompExample.R
+++ data-raw/lengthCompExample.R
@@ -44,6 +44,7 @@ LengthCompExample<-new("LengthComp")
 LengthCompExample@title<-"Example length composition"
 LengthCompExample@description<-"Simulated data using LBSPRsim"
 LengthCompExample@L_units<-"cm"
+LengthCompExample@L_type<-"TL"
 LengthCompExample@dataType<-"freq"
 LengthCompExample@observationUnit<-"Year"
 LengthCompExample@header<-FALSE
