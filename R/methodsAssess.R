fdb258473a132dcd085de6d448ae5f22e3f2ca39
wharford <harford.bill@gmail.com>
2021-12-14 14:40:09 -0500

    Added lbsprWrapper

--- R/methodsAssess.R
+++ R/methodsAssess.R
@@ -174,7 +174,6 @@ PLcFunc<-function(Lc, LengthCompObj, byGroup = FALSE) {
   } else {
 
     #Frequency data
-    #Sum of bin counts >= L50
     if(LengthCompObj@dataType == "Frequency") {
       dt<-poolLengthComp(LengthCompObj, byGroup)
       binNum<-which(dt[,1]>=Lc)
@@ -193,5 +192,123 @@ PLcFunc<-function(Lc, LengthCompObj, byGroup = FALSE) {
         length(len[len>=Lc])/length(len)
       }))
     }
+  }
+}
+
+
+#--------------------------------------------------------------------------------------------------
+#LBSPR
+#--------------------------------------------------------------------------------------------------
+
+#Roxygen header
+#'Estimation of SPR using LBSPR method (Hordyk et al. 2016)
+#'
+#'Hordyk, A.R., Ono, K., Prince, J.D., Walters, C.J., 2016. A simple length-structured model based on life history ratios and incorporating size-dependent selectivity: application to spawning potential ratios for data-poor stocks. Can. J. Fish. Aquat. Sci. 73, 1787–1799. https://doi.org/10.1139/cjfas-2015-0422
+#'
+#' @param LengthCompObj A LengthComp object
+#' @param LifeHistoryObj  A LifeHistory object from fishSimGTG.
+#' @param Lc  A minimum size limit in the same units of measure (L_units) and type of measurement (L_type) as the corresponding LengthComp object. Only used for analysis of fishery-independent length composition data.
+#' @param binWidth  Used to create length-frequencies when LengthComp dataType is raw lengths.
+#' @param cvLinf  Variability in Linf, used in LBSPR fitting. Default value same as that used in LBSPR library
+#' @param byGroup A logical indicating whether quantity is to be calculated separately for each of multiple length comp groups (TRUE) or to length comp is to be pooled across groups prior to calculating quantity (default = FALSE). When TRUE, pooling is ignored if only a single group exists.
+#' @import fishSimGTG LBSPR
+#' @export
+#' @examples
+#' library(fishSimGTG)
+#' library(LBSPR)
+#' lbsprWrapper(fishSimGTG::LifeHistoryExample, fishLengthAssess::LengthCompExample)
+
+
+lbsprWrapper<-function(LifeHistoryObj, LengthCompObj, Lc = 0, binWidth=1, cvLinf = 0.1, byGroup = FALSE) {
+
+  if(!is.numeric(binWidth) ||
+     !is.numeric(cvLinf) ||
+     !is.logical(byGroup) ||
+     !is.numeric(Lc) ||
+     length(LifeHistoryObj@Linf) == 0 ||
+     length(LifeHistoryObj@L50) == 0 ||
+     length(LifeHistoryObj@MK) == 0 ||
+     length(LengthCompObj@dt) == 0 ||
+     length(LengthCompObj@dataType) != 1 ||
+     !(LengthCompObj@dataType %in%  c("Frequency", "Length")) ||
+     !(LengthCompObj@L_source %in%  c("FI", "FD")) ||
+     LifeHistoryObj@Linf < 0 ||
+     LifeHistoryObj@L50 < 0 ||
+     LifeHistoryObj@MK < 0 ||
+     LifeHistoryObj@L50 >= LifeHistoryObj@Linf ||
+     LifeHistoryObj@L50 >= LifeHistoryObj@L95 ||
+     cvLinf < 0 ||
+     LengthCompObj@L_source == "FI" & Lc < 0
+  ) {
+    return(NULL)
+  } else {
+
+    #-------------------------------
+    #Create life history pars list
+    #-------------------------------
+    MyPars <- new("LB_pars")
+    MyPars@CVLinf <- cvLinf
+    MyPars@Linf <- LifeHistoryObj@Linf
+    MyPars@L50 <- LifeHistoryObj@L50
+    MyPars@L95 <- LifeHistoryObj@L95
+    MyPars@MK <- LifeHistoryObj@MK
+    if(length(LifeHistoryObj@LW_A) > 0) MyPars@Walpha <- LifeHistoryObj@LW_A
+    if(length(LifeHistoryObj@LW_B) > 0) MyPars@Wbeta <- LifeHistoryObj@LW_B
+    if(length(LifeHistoryObj@Steep) > 0) MyPars@Steepness<-LifeHistoryObj@Steep
+    if(length(LifeHistoryObj@L_units) >0) MyPars@L_units <- LifeHistoryObj@L_units
+    if(length(LifeHistoryObj@LW_A) >0)  MyPars@Walpha <- LifeHistoryObj@LW_A
+    if(length(LifeHistoryObj@Walpha_units) >0)  MyPars@Walpha_units <-LifeHistoryObj@Walpha_units
+    if(length(LifeHistoryObj@LW_B) >0)   MyPars@Wbeta <- LifeHistoryObj@LW_B
+    if(length(LifeHistoryObj@Lfec_B) >0) {
+      MyPars@FecB <- LifeHistoryObj@Lfec_B
+    } else {
+      MyPars@FecB <- MyPars@Wbeta
+    }
+
+    #-----------------
+    #Data formatting
+    #-----------------
+    #Frequency data
+    if(LengthCompObj@dataType == "Frequency") {
+      dt<-poolLengthComp(LengthCompObj, byGroup)
+      if(LengthCompObj@L_source == "FI") dt<-dt[which(dt[,1] >= Lc),]
+    }
+
+    #If length data
+    if(LengthCompObj@dataType == "Length") {
+      dt<-poolLengthComp(LengthCompObj, byGroup)
+      if(binWidth <= 0) binWidth <- 1
+      Lbins_new<-seq(from=min(dt),
+                     to=max(max((dt) + binWidth), (MyPars@Linf + binWidth)),
+                     by=binWidth
+      )
+      tmp<-sapply(X = 1:NCOL(dt), function(X){
+        if(LengthCompObj@L_source == "FI") dtIn<-dt[which(dt[,1] >= Lc),X]
+        if(LengthCompObj@L_source == "FD") dtIn<-dt[,X]
+        hist(dtIn,Lbins_new,plot=F)$count
+      })
+      dt<-cbind(hist(dt[,1],Lbins_new,plot=F)$mids,tmp)
+    }
+
+    #-----------------
+    #Fitting
+    #-----------------
+    show_condition <- function(code) {
+      tryCatch({
+        x<-code
+        x
+      },  error = function(c) NULL
+      )
+    }
+
+    Len<-new("LB_lengths", dataType="freq", verbose=FALSE)
+    Len@LMids<-dt[,1]
+    Len@LData<-as.matrix(dt[,-1])
+    Len@NYears<-NCOL(dt[,-1])
+    if(LengthCompObj@header) {
+      if(LengthCompObj@dataType == "Frequency")  Len@Years<-colnames(LengthCompObj@dt[,-1])
+      if(LengthCompObj@dataType == "Length") Len@Years<-colnames(LengthCompObj@dt)
+    }
+    return(show_condition(LBSPRfit(LB_pars = MyPars, LB_lengths = Len, Control=list(maxFM=10), verbose=FALSE)))
   }
 }
